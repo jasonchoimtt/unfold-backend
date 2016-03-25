2a2a310d98eaecbd8bc51ad3171893af2777be3b
Jason Choi <jasonchoi.mtt@gmail.com>
2016-03-25 23:43:57 +0800

    hot fix for Post hooks bugging out the scraper

--- src/models/hooks.js
+++ src/models/hooks.js
@@ -7,24 +7,40 @@ import { Publisher, Channels } from '../stream';
 import { Post } from './post';
 
 
-Post.addHook('afterCreate', 'publishToStream', instance => {
+/**
+ * Removes return value of decorated function so that the function does not
+ * return (presumably) a promise.
+ */
+function noWait(asyncFn) {
+    return function(...args) {
+        asyncFn(...args);
+    };
+}
+
+Post.addHook('afterCreate', 'publishToStream', noWait(async instance => {
+    if (!instance.eventId)
+        await instance.reload();
+
     let event = instance.eventId;
     assert(event);
 
-    Publisher.publish(Channels.event(event), JSON.stringify({
+    await Publisher.publish(Channels.event(event), JSON.stringify({
         resource: 'post',
         type: 'created',
         data: instance,
-    })); // async
-});
+    }));
+}));
 
-Post.addHook('afterUpdate', 'publishToStream', instance => {
+Post.addHook('afterUpdate', 'publishToStream', noWait(async instance => {
+    if (!instance.eventId)
+        await instance.reload();
+
     let event = instance.eventId;
     assert(event);
 
-    Publisher.publish(Channels.event(event), JSON.stringify({
+    await Publisher.publish(Channels.event(event), JSON.stringify({
         resource: 'post',
         type: 'updated',
         data: instance,
-    })); // async
-});
+    }));
+}));
